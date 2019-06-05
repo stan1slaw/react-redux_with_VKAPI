f74a427a2bc10aa4bb9e784ba8755f787d69c064
Stanislaw <47894020+stan1slaw@users.noreply.github.com>
2019-06-05 12:41:55 +0300

    Add files via upload

---
+++ src/components/Page.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export class Page extends React.Component {
+    onBtnClick = e => {
+        const year = +e.currentTarget.innerText
+        this.props.getPhotos(year)
+    }
+    renderTemplate = () => {
+        const { photos, isFetching, error } = this.props
+
+        if (error) {
+            return <p className="error">Во время загрузки фото произошла ошибка</p>
+        }
+
+        if (isFetching) {
+            return <p>Загрузка...</p>
+        } else {
+            return photos.map((entry, index) => (
+                <div key={index} className="photo">
+                    <p>
+                        <img src={entry.sizes[0].url} alt="" />
+                    </p>
+                    <p>{entry.likes.count} ❤</p>
+                </div>
+            ))
+        }
+    }
+
+    render() {
+        const { year, photos } = this.props
+        return (
+            <div className="ib page">
+                <p>
+                    <button className="btn" onClick={this.onBtnClick}>
+                        2019
+                    </button>{' '}
+                    <button className="btn" onClick={this.onBtnClick}>
+                        2018
+                    </button>{' '}
+                    <button className="btn" onClick={this.onBtnClick}>
+                        2017
+                    </button>{' '}
+                    <button className="btn" onClick={this.onBtnClick}>
+                        2016
+                    </button>{' '}
+                    <button className="btn" onClick={this.onBtnClick}>
+                        2015
+                    </button>
+
+                </p>
+                <h3>
+                    {year} год [{photos.length}]
+                </h3>
+                {this.renderTemplate()}
+            </div>
+        )
+    }
+}
+
+Page.propTypes = {
+    year: PropTypes.number.isRequired,
+    photos: PropTypes.array.isRequired,
+    getPhotos: PropTypes.func.isRequired,
+    error: PropTypes.string,
+    isFetching: PropTypes.bool.isRequired,
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import { pageReducer} from "./page";
+import { userReducer} from "./user";
+
+export const rootReducer = combineReducers({
+    page: pageReducer,
+    user: userReducer,
+})
+
